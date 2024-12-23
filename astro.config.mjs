91fd741b9c57ed27f794cf25aa61a3a91706ed48
aaron-kz <im.aaronkz@gmail.com>
2024-12-22 21:03:23 -0500

    Add sitemap and Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: squooshImageService(),
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
